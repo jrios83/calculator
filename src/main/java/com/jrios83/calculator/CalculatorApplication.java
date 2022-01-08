dd362b0629e8ca3fb540af277c86cd183648df67
jrios83 <jrios.orsis@gmail.com>
2022-01-08 00:45:01 -0500

    Adding Business logic

--- src/main/java/com/jrios83/calculator/CalculatorApplication.java
+++ src/main/java/com/jrios83/calculator/CalculatorApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
+	int sum(int a, int b) {
+		return a + b;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
