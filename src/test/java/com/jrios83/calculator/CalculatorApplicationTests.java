91c2df80894c830658080101e3a21205eebdc129
jrios83 <jrios.orsis@gmail.com>
2022-01-08 00:04:14 -0500

    Add Spring Boot skeleton

---
+++ src/test/java/com/jrios83/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrios83.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
