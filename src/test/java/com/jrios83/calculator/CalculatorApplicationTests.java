eda20e89291723cfd13b2deabe128665be43073f
jrios83 <jrios.orsis@gmail.com>
2022-01-09 00:22:18 -0500

    Add sum logic, controller and unit test

--- src/test/java/com/jrios83/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/jrios83/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,18 @@
 package com.jrios83.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalculatorApplicationTests {
-
+	private CalculatorApplication calculator = new CalculatorApplication();
 	@Test
-	void contextLoads() {
+	public void testSum() {
+		assertEquals(6, calculator.sum(3, 3));
 	}
+	/*void contextLoads() {
+	}*/
 
 }
